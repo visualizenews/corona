783a9b2fb9bdea1837f168d700b3f78052ecdabc
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-03-09 23:19:01 +0100

    New counter component

---
+++ assets/js/counter/counter.js
@@ -0,0 +1,24 @@
+counter = (data, id) => {
+    const $container = document.querySelector(`#${id}`);
+    
+    console.log(data.italy.regions[data.italy.regions.length-1].data.lombardia.tested);
+
+    const tested = data.italy.global[data.italy.global.length-1].tested;
+    const ratio = population.italy / tested;
+    const lombardy = data.italy.regions[data.italy.regions.length-1].data.lombardia.tested;
+
+    let html = `<div class="counter">
+        <h3 class="counter-title">So far in Italy have been tested</h3>
+        <h2 class="counter-number">${d3.format('.2s')(tested)} <span class="counter-people">people</span></h2>
+        <p class="counter-text">of which, <strong>${d3.format('.2s')(lombardy)}</strong> in Lombardy alone. This means 1 test every <strong>${d3.format('.2s')(ratio)}</strong> italians.</p>
+    </div>`;
+
+    let style = document.createElement('link');
+    style.href = '/assets/css/counter/counter.css';
+    style.rel = 'stylesheet';
+
+    document.querySelector('head').appendChild(style);
+    
+    $container.innerHTML = html;
+    $container.classList.remove('loading');
+}
