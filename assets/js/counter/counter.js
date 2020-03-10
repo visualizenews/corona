ba7d00a42f0e2902ed70333a42eb99090a79968b
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-03-10 20:59:56 +0100

    Counter updates

--- assets/js/counter/counter.js
+++ assets/js/counter/counter.js
@@ -6,15 +6,40 @@ counter = (data, id) => {
     style.rel = 'stylesheet';
     document.querySelector('head').appendChild(style);
 
-    const tested = data.italy.global[data.italy.global.length-1].tested;
-    const ratio = population.italy / tested;
-    const lombardy = data.italy.regions[data.italy.regions.length-1].data.lombardia.tested;
+
+    console.log(data.italy.global[data.italy.global.length-1]);
+
+    const recovered_update = data.italy.global[data.italy.global.length-1].recovered - data.italy.global[data.italy.global.length-2].recovered;
+    const cases_update = data.italy.global[data.italy.global.length-1].cases - data.italy.global[data.italy.global.length-2].cases;
+    const hospital_update = data.italy.global[data.italy.global.length-1].hospital - data.italy.global[data.italy.global.length-2].hospital;
+    const deaths_update = data.italy.global[data.italy.global.length-1].deaths - data.italy.global[data.italy.global.length-2].deaths;
+    const updated = moment(data.generated).format('dddd, MMMM Do YYYY, h:mm a');
 
     let html = `<div class="counter">
-        <h3 class="counter-title">So far in Italy have been tested</h3>
-        <h2 class="counter-number">${d3.format('.2s')(tested)} <span class="counter-people">residents</span></h2>
-        <p class="counter-text">of which, <strong>${d3.format('.2s')(lombardy)}</strong> in Lombardy alone.</p>
-        <p class="counter-text">This means 1 test every <strong>${d3.format('.2s')(ratio)}</strong> italians.</p>
+        <h2 class="counter-title">A quick overview</h2>
+        <div class="counter-wrapper">
+            <div class="counter-column">
+                <h3 class="counter-subtitle">Sick</h3>
+                <h2 class="counter-number">${d3.format('.2s')(data.italy.global[data.italy.global.length-1].cases)} <span class="counter-people">${(cases_update > 0 ? '+' : '')}${d3.format('.2s')(cases_update)}<sup>*</sup></span></h2>
+                <p class="counter-text"><strong>${d3.format('.2s')(data.italy.regions[data.italy.regions.length-1].data.lombardia.cases)}</strong> in Lombardy</p>
+            </div>
+            <div class="counter-column">
+                <h3 class="counter-subtitle">Hospitalized</h3>
+                <h2 class="counter-number">${d3.format('.2s')(data.italy.global[data.italy.global.length-1].hospital)} <span class="counter-people">${(hospital_update > 0 ? '+' : '')}${d3.format('.2s')(hospital_update)}<sup>*</sup></span></h2>
+                <p class="counter-text"><strong>${d3.format('.2s')(data.italy.global[data.italy.global.length-1].icu)}</strong> in ICU</p>
+            </div>
+            <div class="counter-column">
+                <h3 class="counter-subtitle">Deaths</h3>
+                <h2 class="counter-number">${d3.format('.2s')(data.italy.global[data.italy.global.length-1].deaths)} <span class="counter-people">${(deaths_update > 0 ? '+' : '')}${d3.format('.2s')(deaths_update)}<sup>*</sup></span></h2>
+                <p class="counter-text"><strong>${d3.format('.2s')(recovered_update)}</strong> in Lombardy</p>
+            </div>
+            <div class="counter-column">
+                <h3 class="counter-subtitle">Recovered</h3>
+                <h2 class="counter-number">${d3.format('.2s')(data.italy.global[data.italy.global.length-1].recovered)} <span class="counter-people">${(recovered_update > 0 ? '+' : '')}${d3.format('.2s')(recovered_update)}<sup>*</sup></span></h2>
+                <p class="counter-text"><strong>${d3.format('.2s')(data.italy.regions[data.italy.regions.length-1].data.lombardia.recovered)}</strong> in Lombardy</p>
+            </div>
+        </div>
+        <p class="counter-update"><sup>*</sup> Compared to the previous day. Last update: ${updated}.</p>
     </div>`;
     
     $container.innerHTML = html;
