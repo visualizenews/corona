db42fd8688b7359ac278c169e7993f8d09863088
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-03-11 19:48:19 +0100

    cases - recovered

--- assets/js/cases-recovered/index.js
+++ assets/js/cases-recovered/index.js
@@ -22,23 +22,78 @@ casesRecovered = (data, id) => {
             .append('svg')
             .attr('width', width)
             .attr('height', chartHeight)
+
+        const tooltip = container
+            .append('div')
+            .attr('class', 'cases-recovered-tooltip')
+            .attr('id', `${id}-tooltip`)
         
-        const group = svg.append('g')
-               .attr('id', `${id}-group`);
+        const group = svg.append('g');
         
         const axis = svg.append('g')
                 .attr('class', 'axis');
         
+        const annotations = svg.append('g')
+                .attr('class', 'annotations');
+        
+        const maxCases = d3.max(serie1, d => d.y);
+
+        const pixelMatrix = {};
+
+        axis.append('line')
+            .attr('x1', 0)
+            .attr('x2', width)
+            .attr('y1', chartHeight - 38)
+            .attr('y2', chartHeight - 38)
+            .attr('class', 'cases-recovered-axis');
+
         serie1.forEach( (item, index) => {
+            const barX = x(item.x);
+            const barY = y(item.y);
+            const gapY = y(serie2[index].y);
+
+            pixelMatrix[item.x] = {
+                x: barX,
+                y: barY - 5,
+                position: (() => {
+                    if (barX - 90 < 0) {
+                        return 'left';
+                    }
+                    if (barX + 90 > width) {
+                        return 'right';
+                    }
+                    return 'normal';
+                })(),
+                date: moment(item.x).format('MMM, Do'),
+                cases: item.y,
+                recovered: serie2[index].y
+            };
+
             group
                 .append('rect')
                 .attr('width', barWidth)
                 .attr('rx', barWidth / 4)
-                .attr('x', () => x(item.x))
-                .attr('y', () => y(item.y))
-                .attr('height', () => y(serie2[index].y) - y(item.y))
-                .attr('class', 'cases-recovered-rect')
-                .attr('transform', `translate(-${barWidth/2} 0)`);
+                .attr('x', () => barX)
+                .attr('y', () => barY)
+                .attr('height', () => gapY - barY)
+                .attr('class', `cases-recovered-rect ${maxCases === item.y ? 'cases-recovered-rect-max' : ''}`)
+                .attr('transform', `translate(-${barWidth/2} 0)`)
+                .on('mouseover', () => {
+                    tooltip
+                        .html(`<div class="cases-recovered-tooltip-inner">
+                            <span class="cases-recovered-tooltip-date">${pixelMatrix[item.x].date}</span><br />
+                            <span class="cases-recovered-tooltip-data">Active cases: <strong>${d3.format(',')(pixelMatrix[item.x].cases)}</strong></span>
+                            <span class="cases-recovered-tooltip-data">Recovered: <strong>${d3.format(',')(pixelMatrix[item.x].recovered)}</strong></span>
+                        </div>`)
+                        .attr('style', `left: ${pixelMatrix[item.x].x}px; top: ${pixelMatrix[item.x].y}px`)
+                        .attr('class', `cases-recovered-tooltip cases-recovered-tooltip-visible ${pixelMatrix[item.x].position}`)
+                })
+                .on('mouseout', () => {
+                    tooltip
+                        .attr('class', 'cases-recovered-tooltip')
+                        .attr('style', null)
+                        .html('')
+                });
             axis
                 .append('line')
                 .attr('x1', () => x(item.x))
@@ -46,6 +101,7 @@ casesRecovered = (data, id) => {
                 .attr('y1', chartHeight - 38)
                 .attr('y2', chartHeight - 33)
                 .attr('class', 'cases-recovered-tick');
+
             if (index === 0) {
                 axis
                     .append('text')
@@ -56,6 +112,7 @@ casesRecovered = (data, id) => {
                     .attr('alignment-baseline', 'middle')
                     .attr('class', 'cases-recovered-tick-label');
             }
+
             if (index === serie1.length - 1) {
                 axis
                     .append('text')
@@ -68,15 +125,35 @@ casesRecovered = (data, id) => {
             }
         });
 
-        axis.append('line')
+        annotations
+            .append('line')
             .attr('x1', 0)
             .attr('x2', width)
-            .attr('y1', chartHeight - 38)
-            .attr('y2', chartHeight - 38)
-            .attr('class', 'cases-recovered-axis');
+            .attr('y1', y(maxCases))
+            .attr('y2', y(maxCases))
+            .attr('class', 'cases-recovered-top-line');
 
+        annotations
+            .append('text')
+            .text('Maximum daily active')
+            .attr('x', 10)
+            .attr('y', y(maxCases) - 8)
+            .attr('text-anchor', 'start')
+            .attr('alignment-baseline', 'middle')
+            .attr('class', 'cases-recovered-top-label');
 
+        annotations
+            .append('text')
+            .text(` cases: ${d3.format(',')(maxCases)}`)
+            .attr('x', 10)
+            .attr('y', y(maxCases) + 10)
+            .attr('text-anchor', 'start')
+            .attr('alignment-baseline', 'middle')
+            .attr('class', 'cases-recovered-top-label');
         
+
+
+        console.log('pixelMatrix', pixelMatrix);
     };
     
     const updated = moment(data.generated).format('dddd, MMMM Do YYYY, h:mm a');
@@ -97,9 +174,9 @@ casesRecovered = (data, id) => {
     
     $container.innerHTML = html;
 
-    const casesItaly = data.italy.global.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.cases }});
+    const casesItaly = data.italy.global.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.cases - day.deaths }});
     const recoveredItaly = data.italy.global.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.recovered }});
-    const casesLombardy = data.italy.regions.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.data.lombardia.cases }})
+    const casesLombardy = data.italy.regions.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.data.lombardia.cases - day.data.lombardia.deaths }})
     const recoveredLombardy = data.italy.regions.map(day => { return { x: moment(day.datetime).startOf('day').valueOf(), y: day.data.lombardia.recovered }});
     
     const maxYScale = Math.max(d3.max(casesItaly, a => a.y), d3.max(recoveredItaly, a => a.y), d3.max(casesLombardy, a => a.y), d3.max(recoveredLombardy, a => a.y))
