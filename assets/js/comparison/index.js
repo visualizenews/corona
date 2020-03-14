000fc87e2b0ac6a8563df662fcad386bcc5f33fb
carlo <littleark@gmail.com>
2020-03-14 10:44:24 +0000

    line chart component

--- assets/js/comparison/index.js
+++ assets/js/comparison/index.js
@@ -1,45 +1,11 @@
 comparisonChart = (data, id) => {
   const $container = document.querySelector(`#${id}`);
 
+  // new ComparisonChart($container, data);
   new ComparisonChart($container, data);
-
   $container.classList.remove("loading");
 };
-
 function ComparisonChart(container, data, options = {}) {
-  const margin = { top: 20, right: 20, bottom: 30, left: 30 };
-
-  this.width = container.getBoundingClientRect().width;
-  this.height = this.width * (9 / 16);
-
-  const xAxis = g => {
-    g
-      .attr('class','x axis')
-      .attr("transform", `translate(0,${this.height - margin.bottom})`).call(
-      d3
-        .axisBottom(x)
-        .ticks(this.width / 80)
-        .tickSizeOuter(0)
-    );
-  };
-
-  const yAxis = g => {
-    g
-      .attr('class','y axis')
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
-      .call(g => g.select(".domain").remove())
-      .call(g =>
-        g
-          .select(".tick:last-of-type text")
-          .clone()
-          .attr("x", 3)
-          .attr("text-anchor", "start")
-          .attr("font-weight", "bold")
-          .text(data.y)
-      );
-  };
-
   const epicenters = {};
   data.epicenters.forEach(d => {
     Object.entries(d.data).forEach(epicenter => {
@@ -54,98 +20,24 @@ function ComparisonChart(container, data, options = {}) {
       const thisDate = moment(d.datetime);
       epicenters[epicenter[0]].data.push({
         ...epicenter[1],
-        perc: epicenter[1].cases / population[epicenter[0]],
+        perc: epicenter[1].cases / population[epicenter[0]] * 1000000,
         diff: thisDate.diff(startDate, "days")
       });
     });
   });
 
-  // console.log(epicenters);
-
-  const xExtent = d3.extent(
-    [].concat(
-      ...Object.values(epicenters).map(d => d3.extent(d.data, dd => dd.diff))
-    )
-  );
-
-  const x = d3
-    .scaleLinear()
-    .domain(xExtent)
-    .range([margin.left, this.width - margin.right]);
-
-  const yExtent = d3.extent(
-    [].concat(
-      ...Object.values(epicenters).map(d => d3.extent(d.data, dd => dd.perc))
-    )
-  );
-
-  const y = d3
-    .scaleLinear()
-    .domain(yExtent)
-    .nice()
-    .range([this.height - margin.bottom, margin.top]);
-
-  const line = d3
-    .line()
-    .defined(d => !isNaN(d.perc))
-    .x((d, i) => x(d.diff))
-    .y(d => y(d.perc));
-
-  const svg = d3
-    .select(container)
-    .append("svg")
-    .attr("width", this.width)
-    .attr("height", this.height);
-
-  const path = svg
-    .append("g")
-    .selectAll("g")
-    .data(Object.values(epicenters))
-    .join("g")
-    .attr("id", d => d.id)
-    .attr("class", "series")
-    .append("path")
-    //.style("mix-blend-mode", "multiply")
-    .attr("d", d => line(d.data));
-
-  svg.append("g").call(xAxis);
-  // svg.append("g").call(yAxis);
-
-  const updateChart = () => {
-    // console.log("new size", this.width, this.height);
-    x.range([margin.left, this.width - margin.right]);
-    y.range([this.height - margin.bottom, margin.top]);
-
-    svg.attr("width", this.width);
-    svg.attr("height", this.height);
-
-    // svg.selectAll("g.y.axis")
-    //     .call(yAxis);
-    svg.selectAll("g.x.axis")
-        .attr("transform", `translate(0,${this.height - margin.bottom})`)
-        .call(xAxis);
-
-    path.attr("d", d => line(d.data));
-  };
-  if(typeof ResizeObserver !== 'undefined') {
-    const ro = new ResizeObserver(entries => {
-      for (let entry of entries) {
-        if (entry.target === container) {
-          const cr = entry.contentRect;
-          if (cr.width !== this.width) {
-            this.width = cr.width;
-            this.height = this.width * (9 / 16);
-            updateChart();
-          }
-        }
-        // console.log('Element:', entry.target);
-        // console.log(`Element size: ${cr.width}px x ${cr.height}px`);
-        // console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+  new LineChart(epicenters, container, {
+    margin: { top: 20, right: 20, bottom: 30, left: 50 },
+    axes: {
+      x: {
+        field: 'diff',
+        title: 'Day',
+      },
+      y: {
+        field: 'perc',
+        // title: '% on population',
+        title: 'per 1m people'
       }
-    });
-
-    // Observe one or multiple elements
-    ro.observe(container);
-  }
-
+    }
+  })
 }
---
+++ assets/js/components/LineChart.js
@@ -0,0 +1,141 @@
+function LineChart(
+  series,
+  container,
+  options = {
+    axes: {
+      x: {
+        field: 'x',
+        title: '',
+      },
+      y: {
+        field: 'y',
+        title: '',
+      }
+    },
+    margin: { top: 20, right: 20, bottom: 30, left: 30 },
+  }
+) {
+  const { axes, margin, titles } = options;
+
+  console.log('container', container)
+  this.width = container.getBoundingClientRect().width;
+  this.height = this.width * (9 / 16);
+
+  const xExtent = d3.extent(
+    [].concat(
+      ...Object.values(series).map(d =>
+        d3.extent(d.data, dd => dd[axes.x.field])
+      )
+    )
+  );
+
+  const x = d3
+    .scaleLinear()
+    .domain(xExtent)
+    .range([margin.left, this.width - margin.right]);
+
+  const yExtent = d3.extent(
+    [].concat(
+      ...Object.values(series).map(d =>
+        d3.extent(d.data, dd => dd[axes.y.field])
+      )
+    )
+  );
+
+  const y = d3
+    .scaleLinear()
+    .domain(yExtent)
+    .nice()
+    .range([this.height - margin.bottom, margin.top]);
+
+  const line = d3
+    .line()
+    .defined(d => !isNaN(d[axes.y.field]))
+    .x((d, i) => x(d[axes.x.field]))
+    .y(d => y(d[axes.y.field]));
+
+    const xAxis = g => {
+      g.attr("class", "x axis")
+        .attr("transform", `translate(0,${this.height - margin.bottom})`)
+        .call(
+          d3
+            .axisBottom(x)
+            .ticks(this.width / 80)
+            .tickSizeOuter(0)
+        );
+    };
+
+    const yAxis = g => {
+      g.attr("class", "y axis")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g =>
+          g
+            .select(".tick:last-of-type text")
+            .clone()
+            .attr("x", 3)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(axes.y.title)
+        );
+    };
+
+    const svg = d3
+      .select(container)
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height);
+
+    const path = svg
+      .append("g")
+      .selectAll("g")
+      .data(Object.values(series))
+      .join("g")
+      .attr("id", d => d.id)
+      .attr("class", "series")
+      .append("path")
+      //.style("mix-blend-mode", "multiply")
+      .attr("d", d => line(d.data));
+
+    svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis);
+
+    const updateChart = () => {
+      // console.log("new size", this.width, this.height);
+      x.range([margin.left, this.width - margin.right]);
+      y.range([this.height - margin.bottom, margin.top]);
+
+      svg.attr("width", this.width);
+      svg.attr("height", this.height);
+
+      svg.selectAll("g.y.axis")
+          .call(yAxis);
+      svg.selectAll("g.x.axis")
+          .attr("transform", `translate(0,${this.height - margin.bottom})`)
+          .call(xAxis);
+
+      path.attr("d", d => line(d.data));
+    };
+    if(typeof ResizeObserver !== 'undefined') {
+      const ro = new ResizeObserver(entries => {
+        for (let entry of entries) {
+          if (entry.target === container) {
+            const cr = entry.contentRect;
+            if (cr.width !== this.width) {
+              this.width = cr.width;
+              this.height = this.width * (9 / 16);
+              updateChart();
+            }
+          }
+          // console.log('Element:', entry.target);
+          // console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+          // console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+        }
+      });
+
+      // Observe one or multiple elements
+      ro.observe(container);
+    }
+}
