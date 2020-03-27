299632946e9ebbbc138ce20f25ffe6c05f80c8c7
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-03-27 10:24:18 +0100

    New Spacer

--- assets/js/spacers/index.js
+++ assets/js/spacers/index.js
@@ -176,3 +176,40 @@ totalSpacer = (data, id) => {
     window.addEventListener('resize', reset.bind(this));
     $container.classList.remove('loading');
 }
+
+testedSpacer = (data, id) => {
+    const $container = document.querySelector(`#${id}`);
+    const chartData = [];
+
+    console.log(data.italy.global[0]);
+    
+    const prepareData = () => {
+        data.italy.global.forEach((d, i) => {
+            chartData.push({
+                x: moment(d.datetime).valueOf(),
+                y: d.tested,
+            });
+        });
+    }
+
+    const reset = () => {
+        const chartContainer = document.querySelector('#tested-spacer-chart-container');
+        chartContainer.innerHTML = '';
+        console.log(chartData);
+        Spacer({
+            text: 'tested',
+            target: '#tested-spacer-chart-container',
+            maxY: data.italy.global[data.italy.global.length - 1].cases,
+            data: chartData,
+            className: 'total',
+        });
+    }
+
+    let html = `<div class="spacer-chart-container" id="tested-spacer-chart-container"></div>`;
+    
+    prepareData();
+    $container.innerHTML = html;
+    reset();
+    window.addEventListener('resize', reset.bind(this));
+    $container.classList.remove('loading');
+}
